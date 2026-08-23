990e61fd4a85e3a85fd3a609a84ffe011ab8c46b
Tavjeet-Singh13 <tavjeet07@gmail.com>
2026-08-23 23:00:20 +0530

    Create ProductsData.jsx

---
+++ Full Stack/Tech Gear/ProductsData.jsx
@@ -0,0 +1,7 @@
+export const products = [
+    {id: '101', name: 'Product 1',  category: 'audio', price: 5200},
+    {id: '102', name: 'Product 2',  category: 'audio', price: 1200},
+    {id: '103', name: 'Product 3',  category: 'peripherals', price: 2200},
+    {id: '104', name: 'Product 4',  category: 'peripherals', price: 3000},
+    {id: '105', name: 'Product 5',  category: 'display', price: 3200},
+]
